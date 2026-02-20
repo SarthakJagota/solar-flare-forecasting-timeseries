1c523de9810b8b17107d84521cde9b12a68aa6d5
Sarthak Jagota <sarthakjagota34@gmail.com>
2026-02-20 11:45:08 +0530

    Remove pip install command for streamlit

--- solar_deploy.py
+++ solar_deploy.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1NipoW9ZvWwxIxfux-RXlNg_RutCGnmcw
 """
 
-!pip install streamlit
+
 
 import streamlit as st
 import pandas as pd
